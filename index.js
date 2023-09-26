a8a1c26eec33cb2b39736babac91bd3bd0f5575d
Natalja Röhm <roehm.natalia@gmail.com>
2023-09-26 16:22:19 +0200

    Homework7

--- index.js
+++ index.js
@@ -11,5 +11,24 @@ btn.addEventListener('click', () => {
   // ваш код начинается здесь
   
 })
+const btn1 = document.querySelector('#magic-btn');
 
+// создаем новую кнопку с нужными стилями и текстом
+const newBtn = document.createElement('button');
+newBtn.id = 'magic-btn-2';
+newBtn.classList.add('second-btn');
+newBtn.textContent = 'Я изменю тебя';
+newBtn.style.backgroundColor = '#a78b71';
+newBtn.style.color = 'white';
+
+btn.addEventListener('click', () => {
+  // при клике на первую кнопку
+
+  // меняем стили первой кнопки
+  btn.style.backgroundColor = '#9c4a1a';
+  btn.style.color = 'black';
+
+  // добавляем новую кнопку в body
+  document.body.appendChild(newBtn);
+});
 // здесь можете создать EventListener для второй кнопки
